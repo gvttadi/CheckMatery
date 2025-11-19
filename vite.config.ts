b8bac3097efe021dba5ac060aa7d72227a30a7a0
Guilherme Vizioli <gvttadi@gmail.com>
2025-11-19 19:33:47 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/CheckmateMastery/',
+  base: '/CheckmateMatery/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
